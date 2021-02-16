342249ed02f29fb5e7f5ef1de0684e4ff5e387d9
mehrpay <mehrpay@eng.hokudai.ac.jp>
2021-02-17 01:19:29 +0900

    RBSM Tetra Class

---
+++ src/sm/Elements/3D/rbsmtetra.h
@@ -0,0 +1,117 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef rbsmtetra_h
+#define rbsmtetra_h
+
+#include "sm/Elements/structural3delement.h"
+#include "sm/ErrorEstimators/directerrorindicatorrc.h"
+#include "sm/ErrorEstimators/huertaerrorestimator.h"
+#include "zznodalrecoverymodel.h"
+#include "nodalaveragingrecoverymodel.h"
+#include "sprnodalrecoverymodel.h"
+#include "spatiallocalizer.h"
+#include "sm/ErrorEstimators/zzerrorestimator.h"
+#include "mmashapefunctprojection.h"
+
+#define _IFT_RBSMTetra_Name "rbsmtetra"
+
+namespace oofem {
+class FEI3dTetLin;
+
+/**
+ * This class implements a linear tetrahedral four-node finite element for stress analysis.
+ * Each node has 3 degrees of freedom.
+ */
+class RBSMTetra : public Structural3DElement
+                /*,
+                  public ZZNodalRecoveryModelInterface,
+                  public NodalAveragingRecoveryModelInterface,
+                  public SPRNodalRecoveryModelInterface,
+                  public SpatialLocalizerInterface,
+                  public ZZErrorEstimatorInterface,
+                  public HuertaErrorEstimatorInterface
+                */
+{
+protected:
+    static FEI3dTetLin interpolation;
+
+public:
+    RBSMTetra(int n, Domain * d);
+    virtual ~RBSMTetra() { }
+
+    FEInterpolation *giveInterpolation() const override;
+
+    void computeLumpedMassMatrix(FloatMatrix &answer, TimeStep *tStep) override;
+    int giveNumberOfIPForMassMtrxIntegration() override { return 4; }
+    Interface *giveInterface(InterfaceType it) override;
+
+    // definition & identification
+    const char *giveInputRecordName() const override { return _IFT_RBSMTetra_Name; }
+    const char *giveClassName() const override { return "RBSMTetra"; }
+
+#ifdef __OOFEG
+    void drawRawGeometry(oofegGraphicContext &gc, TimeStep *tStep) override;
+    void drawDeformedGeometry(oofegGraphicContext &gc, TimeStep *tStep, UnknownType) override;
+    void drawScalar(oofegGraphicContext &gc, TimeStep *tStep) override;
+    void drawSpecial(oofegGraphicContext &gc, TimeStep *tStep) override;
+#endif
+
+    double giveRelativeSelfComputationalCost() override { return 2.15; }
+
+/*
+    void NodalAveragingRecoveryMI_computeNodalValue(FloatArray &answer, int node,
+                                                    InternalStateType type, TimeStep *tStep) override;
+*/
+
+/*
+    void SPRNodalRecoveryMI_giveSPRAssemblyPoints(IntArray &pap) override;
+    void SPRNodalRecoveryMI_giveDofMansDeterminedByPatch(IntArray &answer, int pap) override;
+    int SPRNodalRecoveryMI_giveNumberOfIP() override;
+    SPRPatchType SPRNodalRecoveryMI_givePatchType() override;
+*/
+
+    // HuertaErrorEstimatorInterface
+/*
+    void HuertaErrorEstimatorI_setupRefinedElementProblem(RefinedElement *refinedElement, int level, int nodeId,
+                                                          IntArray &localNodeIdArray, IntArray &globalNodeIdArray,
+                                                          HuertaErrorEstimatorInterface :: SetupMode sMode, TimeStep *tStep,
+                                                          int &localNodeId, int &localElemId, int &localBcId,
+                                                          IntArray &controlNode, IntArray &controlDof,
+                                                          HuertaErrorEstimator :: AnalysisMode aMode) override;
+    void HuertaErrorEstimatorI_computeNmatrixAt(GaussPoint *gp, FloatMatrix &answer) override;
+*/
+};
+} // end namespace oofem
+#endif // rbsmtetra_h
