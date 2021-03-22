92f82250cae768643289c34db054c0ceb140f75f
saeid <mehrpay@eng.hokudai.ac.jp>
2021-03-23 07:20:16 +0900

    Just alignment of a comment

--- src/oofemlib/element.h
+++ src/oofemlib/element.h
@@ -658,7 +658,7 @@ public:
      */
     void setMaterial(int matIndx) { this->material = matIndx; }
         
-	/**
+    /**
      * Sets the cross section model of receiver.
      * @param csIndx Index of new cross section.
      */
